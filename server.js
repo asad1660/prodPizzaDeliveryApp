6975e7404a67013bd981367dea765cb4be0da492
asad <asadk1660@gmail.com>
2020-09-07 03:16:29 +0500

    fixes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/pizza-delivery-app'));
+app.use(express.static(__dirname + '/dist/pizzaDeliveryApp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/pizza-delivery-app/index.html'));});
+'/dist/pizzaDeliveryApp/index.html'));});
 app.listen(process.env.PORT || 8080);
